eb3ea86f317f59025ae6c087c731d70a13cf2f0c
k0t3n <k0t3n.mail@gmail.com>
2021-05-14 11:59:17 +0300

    Clean up user's admin

--- users/admin.py
+++ users/admin.py
@@ -1,21 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.db.models import Q
 
 from users.models import Friendship, User
 
 
-# TODO
 class FriendshipInline(admin.TabularInline):
     model = Friendship
     fk_name = 'initiator'
     fields = ('id', 'initiator', 'partner')
     extra = 0
-
-    def get_queryset(self, request):
-        return Friendship.objects.filter(
-            Q(partner=request.user) | Q(initiator=request.user)
-        )
 
 
 @admin.register(User)
