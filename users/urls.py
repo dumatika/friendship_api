9efc58b895a130446e046d69e0006d10f5b6f6a6
k0t3n <k0t3n.mail@gmail.com>
2021-05-14 11:53:35 +0300

    Add basename to user's router

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import SimpleRouter
 from users.viewsets import UserViewSet
 
 router = SimpleRouter()
-router.register('', UserViewSet)
+router.register('', UserViewSet, basename='users')
 
 urlpatterns = router.urls
